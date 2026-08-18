a33ae12a19662cef27c4f734a6d42a5e29cbc402
Prathamesh <263696223+prathameshamone@users.noreply.github.com>
2026-08-18 15:43:25 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ThemeProvider>
         <App />
       </ThemeProvider>
